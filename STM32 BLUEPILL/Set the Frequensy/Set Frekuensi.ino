9924d41596cee777498b0e4c58d5db3c32902fcb
Muhammad  Revaldi Frizky <78793603+RevaldiOiek@users.noreply.github.com>
2021-02-24 21:55:21 +0700

    Update Set Frekuensi.ino

--- STM32 BLUEPILL/Set the Frequensy/Set Frekuensi.ino
+++ STM32 BLUEPILL/Set the Frequensy/Set Frekuensi.ino
@@ -1,1 +1,28 @@
-----
+HardwareTimer pwmtimer4(4);
+int pwmValue = 0;
+
+void setup() {
+   pinMode(PB0, INPUT);
+   pinMode(PA0, OUTPUT);
+
+   pwmtimer4.pause();
+   pwmtimer4.setPrescaleFactor(1);
+   pwmtimer4.setOverflow(4235);
+   pwmtimer4.refresh();
+   pwmtimer4.resume();
+
+   pwmWrite(PA0, 0);
+
+   Serial.begin(9600);
+   while (!Serial);
+}
+
+void loop() {
+  if (Serial.available()){
+
+    pwmValue = Serial.parseInt();
+    Serial.println(pwmValue);
+  }
+
+  pwmWrite(PA0, pwmValue);
+}
